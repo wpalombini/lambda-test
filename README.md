ddfdec1e4d9bd693782a87ee56eebc2e31b624c3
Walter Palombini <walterpalombini@gmail.com>
2022-05-14 10:16:40 +1000

    Using serverless framework

--- README.md
+++ README.md
@@ -1,9 +1,35 @@
-# AWS LAMBDA AND LOGGING (CLOUDWATCH) TEST
+# AWS LAMBDA, CLOUDWATCH, API GATEWAY AND SERVERLESS TEST
 
 ## This is a tiny experiment showing how to:
 
-- Package a lambda function
+- Package and deploy a lambda function using serverless framework
 
-- Deploy it to AWS
+- Using api gateway as a trigger for the lambda, again, using serverless framework
 
-- Generate different types (INFO, WARN, ERROR) types of logging in CloudWatch
+- Generate different types of logging in CloudWatch (INFO, WARN, ERROR) with the internal console class
+
+### Commands:
+
+- install dependencies:
+
+```bash
+$ npm install
+```
+
+- run it locally in dev mode:
+
+```bash
+$ npx serverless offline
+```
+
+- run it locally in staging mode:
+
+```bash
+$ npx serverless offline --stage staging
+```
+
+- deploy it to aws:
+
+```bash
+$ npx serverless deploy --stage prod
+```
--- index.js
+++ index.js
@@ -1,14 +1,22 @@
 exports.handler = async (event) => {
   const currentDate = new Date().toLocaleString('en-AU');
 
-  console.log(`=== LOGGING INFO AT ${currentDate}`, { myObj: { info: 'success' } });
+  console.log('environment:', process.env.ENVIRONMENT);
+
+  console.log(`=== LOGGING INFO AT ${currentDate}`, { myObj: { info: 'SUCCESS' } });
 
   console.warn(`=== LOGGING WARN AT ${currentDate}`, { myObj: { warn: 'WARN' } });
 
-  console.error(`=== LOGGING ERROR AT ${currentDate}`, { myObj: { warn: 'ERROR' } });
+  console.error(`=== LOGGING ERROR AT ${currentDate}`, { myObj: { error: 'ERROR' } });
+
+  console.log('event', event);
 
   return {
     statusCode: 200,
-    body: JSON.stringify(`Hello from local lambda at ${currentDate}`),
+    body: JSON.stringify({
+      env: process.env.ENVIRONMENT,
+      message: `Hello from local lambda at ${currentDate}`,
+      event,
+    }),
   };
 };
